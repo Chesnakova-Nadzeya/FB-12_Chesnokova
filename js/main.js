4987f6d3e505749a77271e1575794b6b6806d9fe
chesnokova nadzya <nadzya14@mail.ru>
2021-06-04 18:03:57 +0300

    mobile with burger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 new Swiper('.swiper-container', {
     grabCursor: true,
-    slidesPerView: 3,
     loop: true,
     watchOverflow: true,
     spacebetween: 60,
@@ -8,6 +7,7 @@ new Swiper('.swiper-container', {
     touchRatio: 1,
     slidesPerGroup: 1,
     centerSlides: true,
+    slidesPerView: 3,
 
 
 
@@ -20,15 +20,47 @@ new Swiper('.swiper-container', {
     // effect: 'coverflow',
 
     breakpoints: {
-        360: {
+        849: {
             slidesPerView: 1,
         },
-        480: {
+        1124: {
             slidesPerView: 2,
         },
-        992: {
-            slidesPerView: 3,
-        }
     },
 
 });
+
+
+
+
+
+
+
+
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overlow', 'hidden');
+        } else {
+            $('body').css('overlow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
